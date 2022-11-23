c7825b366d2c8e4767d38895d012c68480cf7eb5
DmitriyV <dmitrio87@mail.ru>
2022-11-23 17:13:41 +0300

    [beginProject] разделение компонента карточки с новостью на несколько общих компонентов

--- my-app/src/components/CardInfoNews/index.tsx
+++ my-app/src/components/CardInfoNews/index.tsx
@@ -1,40 +1,33 @@
 import React from "react";
 import room from "../../image/room.png";
 import "./index.scss";
-import {Link} from 'react-router-dom';
+import {CardDivRight} from '../common/CardDivRight';
+import {CardDivLeft} from '../common/CardDivLeft';
+import {CardDivBottom} from '../common/CardDivBottom';
+import {CardDivCommon} from '../common/CardDivCommon';
 interface CardInfoNewsProps {
   altDescript: string;
   textTitle: string;
   textInfo: string;
-  textDataCard: string;
-  ReadText: string;
 }
 function CardInfoNews({
   altDescript,
   textTitle,
   textInfo,
-  textDataCard,
-  ReadText
 }: CardInfoNewsProps) {
-  function ClickNewsRead(){
-    console.log('переход на страницу с новостью')
-  }
+
   return (
-    <div className="card__div_common">
+    <CardDivCommon>
+   
       <img src={room} alt={altDescript} />
       <p className="card-title__p_center">{textTitle}</p>
       <p className="card-text-info__p">{textInfo}</p>
-      <div className="card__div_bottom">
-        <div className="card__div_left">
-          <p>{textDataCard}</p>
-        </div>
-        <div className="card__div_right">
-          <Link to='/Новость'>
-          <button className="card__button_right" onClick={ClickNewsRead}>{ReadText}</button>
-          </Link>
-        </div>
-      </div>
-    </div>
+      <CardDivBottom>
+        <CardDivLeft textDataCard="14 Января 2008"/>
+        <CardDivRight ReadText="Читать" />
+      </CardDivBottom>
+      </CardDivCommon>
+  
   );
 }
 export default CardInfoNews;
