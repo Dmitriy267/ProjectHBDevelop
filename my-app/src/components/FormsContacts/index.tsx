23772d853fdf78ac66cde0b4e88a8fe88ffc26e1
DmitriyV <dmitrio87@mail.ru>
2022-12-01 21:56:33 +0300

    [formickbranch] добавление react-hook-form на страницу 'Контакты'

--- my-app/src/components/FormsContacts/index.tsx
+++ my-app/src/components/FormsContacts/index.tsx
@@ -1,34 +1,30 @@
 import React, { useState } from "react";
 import styles from './FormsContacts.module.scss';
 import {Label} from '../common/Label';
-import {InputFormContact} from '../common/InputFormContact';
 import User from '../../image/contacts/user.svg';
 import Email from '../../image/contacts/mailform.svg';
 import ModalForm from "../ModalForm";
+import {SubmitHandler, useForm} from 'react-hook-form';
 
+interface IFormInput {
+    name:string;
+    email:string;
+    message:string;
+}
 function FormsContacts(){
+    const {register,handleSubmit}=useForm<IFormInput>();
+    const onSubmit:SubmitHandler<IFormInput>=data=>{
+            console.log(data);
+            setActiveModal(true);  
+    }
     const [activeModal, setActiveModal]=useState(false);
-    const [text, setText]=useState('');
-    const [email, setEmail]=useState('')
-    function handleSubmitModal(event: React.FormEvent<HTMLFormElement>){
-        event.preventDefault();
-        setActiveModal(true)
-    }
-    function handleChangeForm(event: React.ChangeEvent<HTMLTextAreaElement>){
-        setText(event.target.value)
-       console.log(text)
-    }
-    function InputChangeEmail(event:React.ChangeEvent<HTMLInputElement>){
-setEmail(event.target.value)
-console.log(email)
-    }
     return (
-<form className={styles.form} onSubmit={handleSubmitModal}>
+<form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
     <div className={styles.form__div}>
         <div className={styles.group__div}>
-     <div className={styles.formgroup1__div}>   
-    <Label textLabel="Ваше имя"/>
-    <InputFormContact typeText="text"/>
+     <div className={styles.formgroup1__div}>
+     <Label textLabel="Ваше имя"/>     
+   <input type="text" className={styles.form__input} {...register('name',{required:true, maxLength:15})}/>
     </div>
     <div className={styles.user__div}>
     <img src={User} alt='Иконка пользователя' className={styles.user__img}/>
@@ -36,7 +32,8 @@ console.log(email)
 
 <div className={styles.formgroup2__div}>   
     <Label textLabel="Ваша электронная почта" htmlForLab="email"/>
-    <input type="email" placeholder="Введите" name="email" className={styles.form__inputEmail} onChange={InputChangeEmail} value={email} required/>
+    <input type="email" placeholder="Введите" className={styles.form__inputEmail} 
+    {...register('email',{required:true, pattern:/^.+\@.+\..+$/})}/>
     </div>
     <div className={styles.mail__div}>
     <img src={Email} alt='Иконка письма'/>
@@ -44,7 +41,7 @@ console.log(email)
 </div>
 <div className={styles.message__div}>
     <Label textLabel="Ваше сообщение"/>
-        <textarea placeholder="Сообщение" className={styles.form__textarea} value={text} onChange={handleChangeForm}></textarea>
+        <textarea placeholder="Сообщение" className={styles.form__textarea} {...register('message', {required:true, minLength:30})}></textarea>
 </div>
     <input type='submit' value='Отправить' className={styles.form__button}/>
     <ModalForm showModal={activeModal} onClose={()=>setActiveModal(false)}/>
