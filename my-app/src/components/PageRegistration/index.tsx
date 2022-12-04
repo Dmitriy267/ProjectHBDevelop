fec69dbd3c8eee4a77ce49dd5ef349faa0aac703
DmitriyV <dmitrio87@mail.ru>
2022-12-04 16:49:10 +0300

    [formickbranch] добавление проверки формы на страницу регистрации без валидации

--- my-app/src/components/PageRegistration/index.tsx
+++ my-app/src/components/PageRegistration/index.tsx
@@ -1,38 +1,46 @@
 import React from "react";
 import {EnterMainSection} from '../common/EnterMainSection';
 import {PositionModulDiv} from '../common/PositionModulDiv';
-import {ModuleRegistration} from '../common/ModuleRegistration'
-import { ButtonModule } from '../common/ButtonModule';
-import {InputRegistration} from '../common/InputRegistration';
-import styles from './PageRegistration.module.scss';
+import {ModuleRegistration} from '../common/ModuleRegistration';
 import user from '../../image/registration/user.svg';
 import email from '../../image/registration/mail.svg';
 import password from '../../image/registration/password.svg';
+import { ButtonModule } from '../common/ButtonModule';
+import styles from './PageRegistration.module.scss';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+interface InputsRegistrationProps{
+login:string;
+mail:string;
+password:string;
+}
 function PageRegistration (){
+    const { register, handleSubmit } = useForm<InputsRegistrationProps>();
+    const onSubmit: SubmitHandler<InputsRegistrationProps> = data => console.log(data);
     return(
         <EnterMainSection>
             <PositionModulDiv>    
             <ModuleRegistration  width="844px" height="581px" marginTop="175px">
-<form className={styles.registration__form}>
+<form className={styles.registration__form} onSubmit={handleSubmit(onSubmit)}>
     <p>Регистрация</p>
     <div className={styles.registration__div}>
-    <div className={styles.registrationInputs__div}>
-  <InputRegistration type='text' placeholder='Логин' onChange={()=>console.log('input')}/>
-  <img src={user} alt='Иконка пользователя' className={styles.registration__user}/>
-<input type='email' placeholder='Электронная почта'  className={styles.otherEmail__input}/>
-<img src={email} alt='Иконка почты' className={styles.registration__mail}/>
-<InputRegistration type='password' placeholder='Пароль' onChange={()=>console.log('input')}/>
-<img src={password} alt='Иконка пароля' className={styles.registration__password}/>
-<InputRegistration type='password' placeholder='Повторите пароль' onChange={()=>console.log('input')}/>
-<img src={password} alt='Иконка пароля' className={styles.registration__passwordtwo}/>
-<div className={styles.registrationinput__div}>
-    <div className={styles.checkbox__div}>
-<input type='checkbox'/>
-<p>Я не робот</p>
-</div>
-</div>
-<ButtonModule>Зарегистрироваться</ButtonModule>
-    </div>
+        <div className={styles.registrationInputs__div}>
+            <input type='text' placeholder='Логин' {...register('login',{required:true, minLength:8})}/>
+        <img src={user} alt='Иконка пользователя' className={styles.registration__user}/>
+      <input type='email' placeholder='Электронная почта'  className={styles.otherEmail__input} {...register('mail',{required:true, pattern:/^.+\@.+\..+$/})}/>
+      <img src={email} alt='Иконка почты' className={styles.registration__mail}/>
+      <input type='password' placeholder='Пароль' {...register('password',{required:true, minLength:6})}/>
+      <img src={password} alt='Иконка пароля' className={styles.registration__password}/>
+      <input type='password' placeholder='Повторите пароль' {...register('password',{required:true, minLength:6})}/>
+      <img src={password} alt='Иконка пароля' className={styles.registration__passwordtwo}/>
+      <div className={styles.registrationinput__div}>
+          <div className={styles.checkbox__div}>
+      <input type='checkbox'/>
+      <p>Я не робот</p>
+      </div>
+      </div>
+      <ButtonModule>Зарегистрироваться</ButtonModule>
+          </div>
    <div className={styles.rules__div}>
     <div className={styles.rulesUser__div}>
 <p className={styles.rulesUser__p}>Пользователь обязуется:</p>
