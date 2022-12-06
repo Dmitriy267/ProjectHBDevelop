f8cde0629de9007eabe0d3331eca3f96b748cdc4
DmitriyV <dmitrio87@mail.ru>
2022-12-07 00:31:08 +0300

    [hooksbranch] добавление хуков на страницу новостей и попытка отображения новых статей

---
+++ my-app/src/components/ArticleComponent1/index.tsx
@@ -0,0 +1,19 @@
+import React  from "react"
+interface ArticleComponent1Props{
+    imgSrc:string;
+    title:string;
+    description:string;
+    altTitle:string;
+}
+const ArticleComponent1 =({imgSrc, title, description, altTitle}:ArticleComponent1Props)=>{
+    return(
+        <>
+<div>
+<img src={imgSrc} alt={altTitle}/>
+<h3>{title}</h3>
+<p>{description}</p>
+</div>
+</>
+    )
+}
+export default ArticleComponent1;
--- my-app/src/components/PageNewsList/index.tsx
+++ my-app/src/components/PageNewsList/index.tsx
@@ -1,23 +1,48 @@
-import React, {useState} from "react";
+import React, {useState, ChangeEvent, useEffect, MouseEvent, MouseEventHandler } from "react";
 import "./index.scss";
 import home from "../../image/home.svg";
 import loup from "../../image/loup.svg";
 import circle from "../../image/circle.svg";
 import CardInfoNews from '../CardInfoNews';
-import Bathhouse from '../Bathhouse';
+import ArticleComponent1 from '../ArticleComponent1';
 
+const Articles=[{
+  id:1,
+  imgSrc:'',
+  title:'Баня Русская С душой',
+  description:'В Бане С душой вы можете посетить традиционную баню на дровах - проверенную веками и особенно любимую жителями больших городов. Уютный звук сгорающих в печи дров, запах натурального дерева рождают у нас добрые чувства.',
+  altTitle:'Фото бани'
+
+},
+{
+  id:2,
+  imgSrc:'',
+  title:' Банька с веничком на двоих',
+  description:'В сауну Банька с веничком на двоих вы можете прийти веселой компанией, арендовать парную на всех, пообщаться и отдохнуть. У нас вы сможете найти : бодрящую купель, пахучие веники, бильярд, уютную комнату отдыха ',
+  altTitle:'Фото бани'
+
+},
+{
+  id:3,
+  imgSrc:'',
+  title:'Баня На Волге',
+  description:'К Вашим услугам представлены: парная с немецкой дровяной печью (топиться дубовыми дровами). Самый большой бассейн (Саратовское водохранилище).',
+  altTitle:'Фото бани'
+}
+]
 function PageNewsList() {
-  const [article, setArticle]=useState<string>('');
-  const  handeClickArticles = ()=>{
-    console.log(article)
- switch (article){
-  case 'Баня':
-    return <Bathhouse altDescript="Фото бани" textTitle="История и традиции русской бани: краткий очерк" textInfo="Баня всегда была и есть для русского человека не просто местом, где можно принять гигиенические процедуры и очистить свое тело от загрязнений, 
-    а особым, почти сакральным сооружением, где очищение происходит не только на физическом, но и на духовном уровне."/>
-    console.log(article)
- }
+const [inputArticl, setInputArticl]=useState('')
+function onChangeInputArticle (event:ChangeEvent<HTMLInputElement>){
+  return setInputArticl(event.target.value)
+}
+useEffect (()=>{
+ document.title=`Поиск по статье {inputArticl}`
+})
+const onClickArticles:MouseEventHandler=(event)=> {
+console.log('Выбор статьи')
     
   }
+  
   return (
     <section className="page-news__section">
       <div className="block-page-news__div">
@@ -31,26 +56,27 @@ function PageNewsList() {
         <p className="title-news-text__p">Новости </p>
       </div>
       <div className="block-search-articles__div_right">
-        <input type="text" placeholder="Поиск по статьям" value={article} onChange={event=>setArticle(event.target.value)}/>
-        <button onClick={handeClickArticles}>
+        <input type="text" placeholder="Поиск по статьям" value={inputArticl} onChange={onChangeInputArticle} />
+        <button onClick={onClickArticles}>
           <img src={loup} alt="Изображение лупы" />
         </button>
       </div>
-      {
+      <h3>Поиск по статье {inputArticl}</h3>
         <div className="block-articles__div_center">
-      {[...Array(9)].map((item, index)=>
-       <CardInfoNews key={index}
-altDescript="Фото комнаты"
-       textTitle="Линия Сталина: суровый отдых в усадьбах на сутки"
-       textInfo="Чем заняться в выходные? Когда нет безотлагательных домашних дел, а на улице 
-       хорошая погода, хочется уехать из города, чтобы сменить обстановку. Например, снять коттедж на сутки для семьи или большой компании друзей. А..."
-       />
-      )}
-
+        {Articles.map((el, id)=><ArticleComponent1 key={id} imgSrc={el.imgSrc} title={el.title} description={el.description} altTitle={el.altTitle}/>)&&
+          [...Array(9)].map((item, index)=>
+          <CardInfoNews key={index}
+   altDescript="Фото комнаты"
+          textTitle="Линия Сталина: суровый отдых в усадьбах на сутки"
+          textInfo="Чем заняться в выходные? Когда нет безотлагательных домашних дел, а на улице 
+          хорошая погода, хочется уехать из города, чтобы сменить обстановку. Например, снять коттедж на сутки для семьи или большой компании друзей. А..."
+          />
+         )
+       
+         }
+        
       </div>
       
-      
-}
       <ul className="namber-page__ul_bottom">
         <li><a href="#">1</a></li>
         <li><a href="#">2</a></li>
